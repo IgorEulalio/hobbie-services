22294d65612399c473fac7dda77ec1a380beb505
IgorEulalio <igoreulalio.ie@gmail.com>
2020-06-09 22:52:30 -0300

    versão inicial do projeto

---
+++ src/main/java/com/hobbie/services/HobbieServicesApplication.java
@@ -0,0 +1,13 @@
+package com.hobbie.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbieServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbieServicesApplication.class, args);
+	}
+
+}
