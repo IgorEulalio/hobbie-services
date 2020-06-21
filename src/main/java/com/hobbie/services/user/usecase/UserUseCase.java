bc47be98db391f2e58deab1f0140fce67d9a3aa6
IgorEulalio <igoreulalio.ie@gmail.com>
2020-06-21 17:57:09 -0300

    iniciando o desenho de fluxo de feedback

--- src/main/java/com/hobbie/services/user/usecase/UserUseCase.java
+++ src/main/java/com/hobbie/services/user/usecase/UserUseCase.java
@@ -1,6 +1,7 @@
 package com.hobbie.services.user.usecase;
 
 import com.hobbie.services.user.entrypoint.dto.FeedbackDto;
+import com.hobbie.services.user.usecase.model.Feedback;
 import com.hobbie.services.user.usecase.model.User;
 import org.springframework.stereotype.Component;
 
@@ -8,5 +9,18 @@ import org.springframework.stereotype.Component;
 public class UserUseCase {
 
     public void addFeedback(User user, FeedbackDto dto) {
+        Feedback feedback = user.getFeedback();
+
+        if(feedback == null) {
+            Feedback fdbc = Feedback.builder()
+                    .avaliacoes(1)
+                    .nota(Double.valueOf(dto.getNota()))
+                    .build();
+            user.setFeedback(fdbc);
+        }
+        else {
+            feedback.setAvaliacoes(feedback.getAvaliacoes() + 1);
+            feedback.setNota(null);
+        }
     }
 }
