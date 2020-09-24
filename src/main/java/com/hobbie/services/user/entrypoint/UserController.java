c04d332efc76d9a4132f39c5ab98354f3a14c7bf
IgorEulalio <igoreulalio.ie@gmail.com>
2020-09-24 18:53:27 -0300

    Inlcuindo appender kafka e logbook para request/responses

--- src/main/java/com/hobbie/services/user/entrypoint/UserController.java
+++ src/main/java/com/hobbie/services/user/entrypoint/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.zalando.logbook.Logbook;
 
 import java.net.URI;
 import java.util.List;
@@ -16,6 +17,8 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    Logbook logbook = Logbook.create();
 
     @Autowired
     private UserUseCase userUseCase;
