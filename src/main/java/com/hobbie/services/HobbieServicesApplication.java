00ca97e0e21885cc7c6dfdcc5e05002e17125626
IgorEulalio <igoreulalio.ie@gmail.com>
2020-10-05 10:43:37 -0300

    retirando logstash e refinando configuracoes

--- src/main/java/com/hobbie/services/HobbieServicesApplication.java
+++ src/main/java/com/hobbie/services/HobbieServicesApplication.java
@@ -6,15 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.zalando.logbook.HttpLogFormatter;
 import org.zalando.logbook.json.JsonHttpLogFormatter;
-import org.zalando.logbook.logstash.LogstashLogbackSink;
 
 @SpringBootApplication
 public class HobbieServicesApplication {
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-	HttpLogFormatter formatter = new JsonHttpLogFormatter();
-	LogstashLogbackSink sink = new LogstashLogbackSink(formatter);
 
 	public static void main(String[] args) {
 		SpringApplication.run(HobbieServicesApplication.class, args);
