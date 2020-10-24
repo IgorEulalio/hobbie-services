1b51c38d96b78fd40a59548a803f9ea14f67293e
IgorEulalio <igoreulalio.ie@gmail.com>
2020-10-24 01:43:00 -0300

    utilizando log as component

--- src/main/java/com/hobbie/services/user/dataprovider/UserDataProvider.java
+++ src/main/java/com/hobbie/services/user/dataprovider/UserDataProvider.java
@@ -2,13 +2,10 @@ package com.hobbie.services.user.dataprovider;
 
 import com.hobbie.services.user.dataprovider.repository.entity.User;
 import com.hobbie.services.user.dataprovider.repository.UserRepository;
-import com.hobbie.services.user.dataprovider.service.GetUserService;
 import com.hobbie.services.user.entrypoint.dto.UserDTO;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,21 +13,12 @@ import java.util.Optional;
 public class UserDataProvider {
 
     private UserRepository repository;
-    private GetUserService service;
 
-    public UserDataProvider(UserRepository repository, GetUserService service) {
-        this.service = service;
+    public UserDataProvider(UserRepository repository) {
         this.repository = repository;
     }
 
     public User getUserById(String id) {
-
-        try{
-            List<UserDTO> users = service.getUser();
-        }
-        catch (Exception e){
-            System.out.println(e.getMessage());
-        }
 
         Optional<User> byId = repository.findById(id);
 
